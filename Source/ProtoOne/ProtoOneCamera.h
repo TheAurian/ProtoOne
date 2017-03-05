481188c0cc15be8ecd91374b21f505b5f4f6e0f7
Hami <hami@whalefallstudios.com>
2017-03-04 17:40:49 -0800

    created camera source/header files

---
+++ Source/ProtoOne/ProtoOneCamera.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Camera/CameraActor.h"
+#include "ProtoOneCamera.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTOONE_API AProtoOneCamera : public ACameraActor
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
