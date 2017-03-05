3480df1c47d7725263ca5fae8a6d72040705ae9e
Hami <hami@whalefallstudios.com>
2017-03-05 14:58:49 -0800

    Setup Camera class
    - created camera properties and functions

--- Source/ProtoOne/ProtoOneCamera.cpp
+++ Source/ProtoOne/ProtoOneCamera.cpp
@@ -6,3 +6,20 @@
 
 
 
+AProtoOneCamera::AProtoOneCamera() {
+
+
+
+	/*
+	// Create a camera boom (pulls in towards the player if there is a collision)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
+	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+
+	// Create a follow camera
+	PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
+	PlayerCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+	PlayerCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm */
+}
+
--- Source/ProtoOne/ProtoOneCamera.h
+++ Source/ProtoOne/ProtoOneCamera.h
@@ -12,8 +12,63 @@ UCLASS()
 class PROTOONE_API AProtoOneCamera : public ACameraActor
 {
 	GENERATED_BODY()
+
+
+	/** Arm connecting the character to the Usphere*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class USpringArmComponent* PlayerBoom;
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class USpringArmComponent* CameraBoom;
+
+	/** Main camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class UCameraComponent* PlayerCamera;
+
+	/** Sphere that that camera uses determine when to follow */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class USphereComponent* CameraSphere;
+
+public:
+
+	AProtoOneCamera();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	ACharacter* CameraTarget;
 	
-	
-	
-	
+	/** Returns PlayerBoom object*/
+	FORCEINLINE USpringArmComponent* GetPlayerBoom() const { return PlayerBoom; };
+
+	/** Returns CameraBoom object*/
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; };
+
+	/** Returns PlayerCamera object*/
+	FORCEINLINE UCameraComponent* GetPlayerCamera() const { return PlayerCamera; };
+
+	/** Returns CameraSphere object*/
+	FORCEINLINE USphereComponent* GetCameraSphere() const { return CameraSphere; };
+
+
+protected:
+
+	/** The size of the sphere around the character */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Camera)
+		float SphereSize;
+
+	/** The time the camera will wait before recentering on the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Camera)
+		float RecenterDelay;
+
+	/** The distance or length of boom between the character and the center of the CameraSphere */
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Camera)
+		float PlayerBoomDistance;
+
+	/** The distance or length of boom between the camera and the center of the CameraSphere */
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Camera)
+		float CameraBoomDistance;
+
+private:
+
+
 };
