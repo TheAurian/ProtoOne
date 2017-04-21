57d8d2a2fd439bf856098e5eff491e57fc7f0e66
Hami <hami@whalefallstudios.com>
2017-04-20 18:56:22 -0700

    UI 2.1: UI Text
    - added progress bar for synergy
    -  added text the health and synergy bars

--- Source/ProtoOne/ProtoOneGameMode.h
+++ Source/ProtoOne/ProtoOneGameMode.h
@@ -14,11 +14,25 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 		class AProtoOneCamera* MainCamera;
 
+	UFUNCTION(BlueprintPure, category = "HUD")
+		FString GetHUDHealth() { return HUD_Health; };
+
+	UFUNCTION(BlueprintPure, category = "HUD")
+		FString GetHUDSynergy() { return HUD_Synergy; };
+
+
+
 	virtual void BeginPlay() override;
 
 protected:
 
 	/** HUD Attributes*/
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, category = "HUD", meta = (BlueprintProtected = "true"))
+		FString HUD_Health;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, category = "HUD", meta = (BlueprintProtected = "true"))
+		FString HUD_Synergy;
 
 	//UE4 macro included for garbage collection only (need no other keywords)
 	UPROPERTY()
